29f9859b604fe04b0c9eaa25fd26ad5931f96bb2
Cataline Baert <snuf_kin@yahoo.com>
2022-04-18 21:02:00 +0200

    Define create action in invitations controller and display success or error message on form submit

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -2,5 +2,26 @@ class InvitationsController < ApplicationController
   def new
     @cycle = Cycle.find(params[:cycle_id])
     @invitation = Invitation.new
+
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def create
+    @cycle = Cycle.find(params[:cycle_id])
+    @invitation = Invitation.new(invitation_params)
+    @invitation.cycle = @cycle
+    @invitation.save
+
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  private
+
+  def invitation_params
+    params.require(:invitation).permit(:email, :message)
   end
 end
