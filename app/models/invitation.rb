d4e7acca3e9b30dd992002e61e245cdd6ed037b5
Cataline Baert <snuf_kin@yahoo.com>
2022-04-21 17:29:10 +0200

    Simplify extraction of name and email

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,18 +1,17 @@
 class Invitation < ApplicationRecord
-  EMAIL_WITH_NAME_PATTERN = /(?<name>[a-zA-Z\s]+)<(?<email>[^@\s]+@[-a-z0-9]+\.+[a-z]{2,})>/i
-  ONLY_EMAIL_PATTERN = /(?<username>[^@\s]+)@[-a-z0-9]+\.+[a-z]{2,}/i
-  VALIDATION_PATTERN = Regexp.union(EMAIL_WITH_NAME_PATTERN, ONLY_EMAIL_PATTERN)
+  EMAIL_WITH_NAME_PATTERN = /\A(?<name>[a-zA-Z\s]+)<(?<email>[^@\s]+@[^@\s]+)>\z/i
+  EMAIL_PATTERN = /\A[^@\s]+@[-a-z0-9]+\.+[a-z]{2,}\z/i
 
   belongs_to :cycle
 
   validates_presence_of :email
-  validates_format_of :email, with: VALIDATION_PATTERN
+  validates_format_of :email, with: EMAIL_PATTERN
 
-  before_create :extract_name_and_email
+  before_validation :extract_name_and_email, unless: proc { email.empty? }
 
   def extract_name_and_email
-    match_data = email.match(VALIDATION_PATTERN)
-    return self.name = generate_name_from(match_data[:username]) if match_data[:name].nil?
+    match_data = email.match(EMAIL_WITH_NAME_PATTERN)
+    return self.name = generate_name_from(email) if match_data.nil? && !email.empty?
 
     self.name = match_data[:name].strip
     self.email = match_data[:email]
@@ -20,7 +19,7 @@ class Invitation < ApplicationRecord
 
   private
 
-  def generate_name_from(username)
-    username.split('.').map(&:capitalize).join(' ').tr('0-9', '').strip
+  def generate_name_from(email_input)
+    email_input.split('@').first.split('.').map(&:capitalize).join(' ').tr('0-9', '').strip
   end
 end
